d6b9a8bb7b8fda26858dda31a643b7c2579f97fd
falcondev <tinhtcp@gmail.com>
2024-10-30 11:49:16 -0600

    Update premake file

--- premake5.lua
+++ premake5.lua
@@ -26,5 +26,13 @@ project "ImGui"
     	cppdialect "C++17"
 		staticruntime "On" --staticly linking the runtime libraries
 
+	filter "configurations: Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations: Release"
+		runtime "Release"
+		symbols "on"
+
 	filter { "system:windows", "configurations:Release" }
 		buildoptions "/MT"

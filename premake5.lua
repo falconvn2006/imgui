a7bfd046f37fce91004ed27328e77b3d1d243a2e
Bùi Trung Tín <88769526+falconvn2006@users.noreply.github.com>
2024-11-07 11:12:33 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,7 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
-	staticruntime "On" --staticly linking the runtime libraries
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -12,23 +11,29 @@ project "ImGui"
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
-    	"imgui_draw.cpp",
-    	"imgui_internal.h",
-    	"imgui_widgets.cpp",
-    	"imgui_tables.cpp",
-    	"imstb_rectpack.h",
-    	"imstb_textedit.h",
-    	"imstb_truetype.h",
-    	"imgui_demo.cpp"
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
+    	cppdialect "C++17"
 
-	filter "configurations: Debug"
+	filter "configuration:Debug"
 		runtime "Debug"
-		symbols "on"
+		symbols "On"
 
-	filter "configurations: Release"
+	filter "configuration:Release"
 		runtime "Release"
-		symbols "on"
+		symbols "On"
+
+	filter "configuration:Dist"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
